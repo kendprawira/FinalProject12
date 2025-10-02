2f92b05e86ae71a000f079eb23ad8d4f37d0ec7a
ferthereaper <ferdyansahprakoso55@gmail.com>
2025-10-02 15:43:42 +0700

    tugasanu

--- Test1218.java
+++ Test1218.java
@@ -1,1 +1,6 @@
-
+public class Test1218 {
+    public static void main(String[] args) {
+        System.out.println("Hi, ini baris kode pertama yang dimodifikasi Ferdy12");
+        System.out.println("Ferdy12 menambahkan baris baru");
+    }
+}
