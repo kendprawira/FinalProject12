bd54cfa3302311922fa0e3d59e04f8f941e91748
kendprawira <kendprawira@gmail.com>
2025-10-02 15:55:34 +0700

    new comment

--- Test1218.java
+++ Test1218.java
@@ -1,6 +1,6 @@
 public class Test1218 {
     public static void main(String[] args) {
         System.out.println("Hi, ini baris kode pertama yang dimodifikasi ken18");
-        System.out.println("ken18 menambahkan baris bar");
+        System.out.println("ken18 menambahkan baris bar"); // edit tambah comment
     }
 }
