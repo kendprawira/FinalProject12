cd4ebae36dd1bdf55bb172d2994b038ca835f206
kendprawira <kendprawira@gmail.com>
2025-10-02 15:48:48 +0700

    ken2

--- Test1218.java
+++ Test1218.java
@@ -1,6 +1,6 @@
 public class Test1218 {
     public static void main(String[] args) {
         System.out.println("Hi, ini baris kode pertama yang dimodifikasi ken18");
-        System.out.println("ken18 menambahkan baris baru");
+        System.out.println("ken18 menambahkan baris bar");
     }
 }
